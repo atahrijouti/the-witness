94dd1ff946704088f18fc5c04f23684b93c20da6
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2016-02-01 16:43:35 +0000

    initial structure

---
+++ app/js/game.js
@@ -0,0 +1,10 @@
+define(["require", "exports"], function (require, exports) {
+    var Game = (function () {
+        function Game() {
+        }
+        return Game;
+    })();
+    exports.__esModule = true;
+    exports["default"] = new Game();
+});
+//# sourceMappingURL=game.js.map
